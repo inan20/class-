cf17b60e1a8e999455f6d240c6f494619f0fcdbb
inan20 <ahmetinanyunefa@gmail.com>
2022-08-19 11:52:49 +0300

    A1

--- src/C01_Stash.java
+++ src/C01_Stash.java
@@ -2,6 +2,7 @@ public class C01_Stash {
     public static void main(String[] args) {
         System.out.println("github 1.  master deneme");
         System.out.println("github 2.  master deneme ");
+        System.out.println("github  1. ahmet  deneme");
 
 
     }
